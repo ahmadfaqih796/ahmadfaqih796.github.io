70256a6ba5938f42008effd09de0151189d26967
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-08-09 15:02:57 +0700

    update

--- assets/js/navigasi.js
+++ assets/js/navigasi.js
@@ -1,8 +1,22 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 500);
 });
-var $li = $('nav ul li a').click(function() {
+var $li = $('nav ul li a').click(function () {
     $li.removeClass('selected');
     $(this).addClass('selected');
+});
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 700);
+                return false;
+            }
+        }
+    });
 });
